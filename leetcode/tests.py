6d83b7ae158ad9c2594a4a2fc70b19c61a635a63
Harry <harrisonhemstreet@proton.me>
2023-12-05 21:23:49 -0700

    solved #26

--- leetcode/tests.py
+++ leetcode/tests.py
@@ -4,6 +4,7 @@ from palindrome_number import PalindromeNumber
 from roman_to_int import RomanToInt
 from longest_common_prefix import LongestCommonPrefix
 from valid_parens import ValidParens
+from remove_duplicates import RemoveDups
 
 class TestSolution(unittest.TestCase):
     def setUp(self):
@@ -12,6 +13,7 @@ class TestSolution(unittest.TestCase):
         self.roman_to_int = RomanToInt()
         self.longest_common_prefix = LongestCommonPrefix()
         self.valid_parens = ValidParens()
+        self.remove_dups = RemoveDups()
 
     def test_two_sum(self):
         self.assertEqual(self.two_sum.run([2,7,11,15], 9), [0,1])
@@ -40,6 +42,12 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(self.valid_parens.run("(]"), False)
         self.assertEqual(self.valid_parens.run("([)]"), False)
         self.assertEqual(self.valid_parens.run("({[]})"), True)
+    
+    def test_remove_dups(self):
+        self.assertEqual(self.remove_dups.run([1,1,2,3,4,4]), 4)
+        self.assertEqual(self.remove_dups.run([1,2,2,3,4,4]), 4)
+        self.assertEqual(self.remove_dups.run([1,2]), 2)
+        self.assertEqual(self.remove_dups.run([0,0,1,1,1,2,2,3,3,4]), 5)
 
 if __name__ == '__main__':
     unittest.main()
