ec8843d7c03a6a68fce61401945105b8a774a934
Harry <harrisonhemstreet@proton.me>
2023-12-06 17:14:13 -0700

    06 December 2023

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -1,60 +1,39 @@
 # 06 December 2023
 
 # Time:  O(n)
-# Space: O(1)
+# Space: O(h)
 
-import heapq
+class Node(object):
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+
 
 class Solution(object):
-    # @param {integer} n
-    # @return {integer}
-    def nthUglyNumber(self, n):
-        ugly_number = 0
-
-        heap = []
-        heapq.heappush(heap, 1)
-        for _ in xrange(n):
-            ugly_number = heapq.heappop(heap)
-            if ugly_number % 2 == 0:
-                heapq.heappush(heap, ugly_number * 2)
-            elif ugly_number % 3 == 0:
-                heapq.heappush(heap, ugly_number * 2)
-                heapq.heappush(heap, ugly_number * 3)
-            else:
-                heapq.heappush(heap, ugly_number * 2)
-                heapq.heappush(heap, ugly_number * 3)
-                heapq.heappush(heap, ugly_number * 5)
-
-        return ugly_number
-
-    def nthUglyNumber2(self, n):
-        ugly = [1]
-        i2 = i3 = i5 = 0
-        while len(ugly) < n:
-            while ugly[i2] * 2 <= ugly[-1]: i2 += 1
-            while ugly[i3] * 3 <= ugly[-1]: i3 += 1
-            while ugly[i5] * 5 <= ugly[-1]: i5 += 1
-            ugly.append(min(ugly[i2] * 2, ugly[i3] * 3, ugly[i5] * 5))
-        return ugly[-1]
-
-    def nthUglyNumber3(self, n):
-        q2, q3, q5 = [2], [3], [5]
-        ugly = 1
-        for u in heapq.merge(q2, q3, q5):
-            if n == 1:
-                return ugly
-            if u > ugly:
-                ugly = u
-                n -= 1
-                q2 += 2 * u,
-                q3 += 3 * u,
-                q5 += 5 * u,
-
-
-class Solution2(object):
-    ugly = sorted(2**a * 3**b * 5**c
-                  for a in range(32) for b in range(20) for c in range(14))
-
-    def nthUglyNumber(self, n):
-        return self.ugly[n-1]
+    def construct(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: Node
+        """
+        def dfs(grid, x, y, l):
+            if l == 1:
+                return Node(grid[x][y] == 1, True, None, None, None, None)
+            half = l // 2
+            topLeftNode = dfs(grid, x, y, half)
+            topRightNode = dfs(grid, x, y+half, half)
+            bottomLeftNode = dfs(grid, x+half, y, half)
+            bottomRightNode = dfs(grid, x+half, y+half, half)
+            if topLeftNode.isLeaf and topRightNode.isLeaf and \
+               bottomLeftNode.isLeaf and bottomRightNode.isLeaf and \
+               topLeftNode.val == topRightNode.val == bottomLeftNode.val == bottomRightNode.val:
+                return Node(topLeftNode.val, True, None, None, None, None)
+            return Node(True, False, topLeftNode, topRightNode, bottomLeftNode, bottomRightNode)
+        
+        if not grid:
+            return None
+        return dfs(grid, 0, 0, len(grid))
 
