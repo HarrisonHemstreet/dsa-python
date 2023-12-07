f07e464461ec67bf1b748153678405ea72c32e6c
Harry <harrisonhemstreet@proton.me>
2023-12-07 13:55:56 -0700

    finished daily #1903

--- leetcode/tests.py
+++ leetcode/tests.py
@@ -11,6 +11,7 @@ from search_insert import SearchInsert
 from length_of_last_word import LengthLastWord
 from plus_one import PlusOne
 from add_binary import AddBinary
+from largest_odd_num_in_str import LargestOddNumInStr
 
 class TestSolution(unittest.TestCase):
     def setUp(self):
@@ -26,22 +27,23 @@ class TestSolution(unittest.TestCase):
         self.length_last_word = LengthLastWord()
         self.plus_one = PlusOne()
         self.add_binary = AddBinary()
+        self.largest_odd_num_in_str = LargestOddNumInStr()
 
     def test_two_sum(self):
         self.assertEqual(self.two_sum.run([2,7,11,15], 9), [0,1])
         self.assertEqual(self.two_sum.run([3,2,4], 6), [1,2])
         self.assertEqual(self.two_sum.run([3,3], 6), [0,1])
-    
+
     def test_palindrome_number(self):
         self.assertEqual(self.palindrome_number.run(121), True)
         self.assertEqual(self.palindrome_number.run(-121), False)
         self.assertEqual(self.palindrome_number.run(123), False)
-    
+
     def test_roman_to_int(self):
         self.assertEqual(self.roman_to_int.run("III"), 3)
         self.assertEqual(self.roman_to_int.run("LVIII"), 58)
         self.assertEqual(self.roman_to_int.run("MCMXCIV"), 1994)
-    
+
     def test_longest_common_prefix(self):
         self.assertEqual(self.longest_common_prefix.run(["flower","flow","flight"]), "fl")
         self.assertEqual(self.longest_common_prefix.run(["dog","racecar","car"]), "")
@@ -54,22 +56,22 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(self.valid_parens.run("(]"), False)
         self.assertEqual(self.valid_parens.run("([)]"), False)
         self.assertEqual(self.valid_parens.run("({[]})"), True)
-    
+
     def test_remove_dups(self):
         self.assertEqual(self.remove_dups.run([1,1,2,3,4,4]), 4)
         self.assertEqual(self.remove_dups.run([1,2,2,3,4,4]), 4)
         self.assertEqual(self.remove_dups.run([1,2]), 2)
         self.assertEqual(self.remove_dups.run([0,0,1,1,1,2,2,3,3,4]), 5)
-    
+
     def test_remove_element(self):
         self.assertEqual(self.remove_element.run([3,2,2,3], 3), 2)
         self.assertEqual(self.remove_element.run([0,1,2,2,3,0,4,2], 2), 5)
-    
+
     def test_first_occurrence(self):
         self.assertEqual(self.first_occurrence.run("bear", "ear"), 1)
         self.assertEqual(self.first_occurrence.run("ted", "ed"), 1)
         self.assertEqual(self.first_occurrence.run("bear eat bois", "aoeu"), -1)
-    
+
     def test_search_insert(self):
         self.assertEqual(self.search_insert.run([1,3,5,6], 5), 2)
         self.assertEqual(self.search_insert.run([1,3,5,6], 2), 1)
@@ -77,21 +79,27 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(self.search_insert.run([1], 0), 0)
         self.assertEqual(self.search_insert.run([1], 1), 0)
         self.assertEqual(self.search_insert.run([1,3], 2), 1)
-    
+
     def test_length_last_word(self):
         self.assertEqual(self.length_last_word.run("Hello World"), 5)
         self.assertEqual(self.length_last_word.run("   fly me   to   the moon  "), 4)
         self.assertEqual(self.length_last_word.run("luffy is still joyboy"), 6)
-    
+
     def test_plus_one(self):
         self.assertEqual(self.plus_one.run([1,2,3]), [1,2,4])
         self.assertEqual(self.plus_one.run([4,3,2,1]), [4,3,2,2])
         self.assertEqual(self.plus_one.run([9]), [1,0])
         self.assertEqual(self.plus_one.run([1,0]), [1,1])
-    
+
     def test_add_binary(self):
         self.assertEqual(self.add_binary.run("11", "1"), "100")
         self.assertEqual(self.add_binary.run("1010", "1011"), "10101")
+
+    def test_largest_odd_num_in_str(self):
+        self.assertEqual(self.largest_odd_num_in_str.run("52"), "5")
+        self.assertEqual(self.largest_odd_num_in_str.run("4206"), "")
+        self.assertEqual(self.largest_odd_num_in_str.run("35427"), "35427")
+        self.assertEqual(self.largest_odd_num_in_str.run("10133890"), "1013389")
 
 if __name__ == '__main__':
     unittest.main()
