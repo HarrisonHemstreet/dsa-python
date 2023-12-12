015b7fd654068867d10af8abc11ee9c9514e7cbb
Harry <harrisonhemstreet@proton.me>
2023-12-12 10:53:01 -0700

    12 December 2023

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -190,3 +190,36 @@ class Solution(object):
         backtracking(new_s, k, [], cnts, result)
         return "".join(result)
 
+# 12 December 2023
+
+# Time:  O(nlogn)
+# Space: O(n)
+
+from collections import Counter
+from heapq import heapify, heappop
+
+
+class Solution(object):
+    def isNStraightHand(self, hand, W):
+        """
+        :type hand: List[int]
+        :type W: int
+        :rtype: bool
+        """
+        if len(hand) % W:
+            return False
+
+        counts = Counter(hand)
+        min_heap = list(hand)
+        heapify(min_heap)
+        for _ in xrange(len(min_heap)//W):
+            while counts[min_heap[0]] == 0:
+                heappop(min_heap)
+            start = heappop(min_heap)
+            for _ in xrange(W):
+                counts[start] -= 1
+                if counts[start] < 0:
+                    return False
+                start += 1
+        return True
+
