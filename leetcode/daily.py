5ef9260651b596722a0a800078df7a3920936b60
Harry <harrisonhemstreet@proton.me>
2023-12-15 00:00:01 -0700

    15 December 2023

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -308,3 +308,23 @@ class Solution(object):
         nth_element(arr, k-1, compare=lambda a, b: a > b)
         return arr[k-1]
 
+# 15 December 2023
+
+# Time:  O(n^2)
+# Space: O(n^2)
+
+class Solution(object):
+    # @param s, a string
+    # @return an integer
+    def minCut(self, s):
+        lookup = [[False for j in xrange(len(s))] for i in xrange(len(s))]
+        mincut = [len(s) - 1 - i for i in xrange(len(s) + 1)]
+
+        for i in reversed(xrange(len(s))):
+            for j in xrange(i, len(s)):
+                if s[i] == s[j]  and (j - i < 2 or lookup[i + 1][j - 1]):
+                    lookup[i][j] = True
+                    mincut[i] = min(mincut[i], mincut[j + 1] + 1)
+
+        return mincut[0]
+
