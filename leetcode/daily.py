ab291b2ec9a94605361e9bfbf5c240e8f94e6a7b
Harry <harrisonhemstreet@proton.me>
2023-12-21 00:00:01 -0700

    21 December 2023

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -486,3 +486,24 @@ class Solution(object):
         result = []
         return result if dfs(root, voyage, [0], result) else [-1]
 
+# 21 December 2023
+
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    # @param A, a list of integers
+    # @return an integer
+    def firstMissingPositive(self, A):
+        i = 0
+        while i < len(A):
+            if A[i] > 0 and A[i] - 1 < len(A) and A[i] != A[A[i]-1]:
+                A[A[i]-1], A[i] = A[i], A[A[i]-1]
+            else:
+                i += 1
+
+        for i, integer in enumerate(A):
+            if integer != i + 1:
+                return i + 1
+        return len(A) + 1
+
