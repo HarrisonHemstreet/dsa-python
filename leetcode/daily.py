904e83cb6f2430627a162e96746cf339c9066a50
Harry <harrisonhemstreet@proton.me>
2023-12-23 00:00:01 -0700

    23 December 2023

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -559,3 +559,29 @@ class Solution3(object):
         """
         return sum(sum(map(int, str(x)))%2 == 0 for x in xrange(1, num+1))
 
+# 23 December 2023
+
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+
+
+class Solution(object):
+    def getDistances(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        lookup = collections.defaultdict(list)
+        for i, x in enumerate(arr):
+            lookup[x].append(i)
+        result = [0]*len(arr)
+        for idxs in lookup.itervalues():
+            prefix = [0]
+            for i in idxs:
+                prefix.append(prefix[-1]+i)
+            for i, idx in enumerate(idxs):
+                result[idx] = (idx*(i+1)-prefix[i+1]) + ((prefix[len(idxs)]-prefix[i])-idx*(len(idxs)-i))
+        return result
+
