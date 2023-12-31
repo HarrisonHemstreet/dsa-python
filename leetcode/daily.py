ab0de2eff69855a75f41fd62e8606dc454450888
Harry <harrisonhemstreet@proton.me>
2023-12-31 13:17:01 -0700

    31 December 2023

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -799,3 +799,34 @@ class Solution(object):
                 left, right, l, pow_D = 0, 0, 0, 1
         return result
 
+# 31 December 2023
+
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+
+
+class Solution(object):
+    def waysToPartition(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        total = sum(nums)
+        right = collections.Counter()
+        prefix = 0
+        for i in xrange(len(nums)-1):
+            prefix += nums[i]
+            right[prefix-(total-prefix)] += 1
+        result = right[0]
+        left = collections.Counter()
+        prefix = 0
+        for x in nums:
+            result = max(result, left[k-x]+right[-(k-x)])
+            prefix += x
+            left[prefix-(total-prefix)] += 1
+            right[prefix-(total-prefix)] -= 1
+        return result
+
