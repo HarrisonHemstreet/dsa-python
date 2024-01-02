2f2d0f65a3fe7cc2430d144693e66157f6aba772
Harry <harrisonhemstreet@proton.me>
2024-01-02 00:00:02 -0700

    02 January 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -879,3 +879,26 @@ class Solution(object):
                 left = mid+1
         return fonts[right] if right >= 0 else -1
 
+# 02 January 2024
+
+# Time:  O(n)
+# Space: O(k)
+
+# constructive algorithms
+class Solution(object):
+    def shortestSequence(self, rolls, k):
+        """
+        :type rolls: List[int]
+        :type k: int
+        :rtype: int
+        """
+        l = 0
+        lookup = set()
+        for x in rolls:
+            lookup.add(x)
+            if len(lookup) != k:
+                continue
+            lookup.clear()
+            l += 1
+        return l+1
+
