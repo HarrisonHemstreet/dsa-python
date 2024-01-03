f2d2f36e4c998491be636e756faff06120fd503e
Harry <harrisonhemstreet@proton.me>
2024-01-03 00:00:01 -0700

    03 January 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -902,3 +902,29 @@ class Solution(object):
             l += 1
         return l+1
 
+# 03 January 2024
+
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def minimumBuckets(self, street):
+        """
+        :type street: str
+        :rtype: int
+        """
+        result = 0
+        street = list(street)
+        for i, c in enumerate(street):
+            if c != 'H' or (i and street[i-1] == 'B'):
+                continue
+            if i+1 < len(street) and street[i+1] == '.':
+                street[i+1] = 'B'
+                result += 1
+            elif i and street[i-1] == '.':
+                street[i-1] = 'B'
+                result += 1
+            else:
+                return -1
+        return result
+
