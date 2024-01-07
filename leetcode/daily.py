f175da88c8136e9e8bc1382e3adfbf7760641e49
Harry <harrisonhemstreet@proton.me>
2024-01-06 22:58:16 -0700

    06 January 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -1051,3 +1051,25 @@ class Solution(object):
                 left = mid+1
         return left
 
+# 07 January 2024
+
+# Time:  O(nlogn)
+# Space: O(1)
+
+class Solution(object):
+    def eliminateMaximum(self, dist, speed):
+        """
+        :type dist: List[int]
+        :type speed: List[int]
+        :rtype: int
+        """
+        for i in xrange(len(dist)):
+            dist[i] = (dist[i]-1)//speed[i]
+        dist.sort()
+        result = 0
+        for i in xrange(len(dist)):
+            if result > dist[i]:
+                break
+            result += 1
+        return result
+
