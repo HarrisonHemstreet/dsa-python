3ca93bcef676af559e6ca8fa643fbad9392e3cff
Harry <harrisonhemstreet@proton.me>
2024-01-09 00:56:02 -0700

    09 January 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -1150,3 +1150,32 @@ class Solution(object):
                         left += 1
         return result
 
+# 09 January 2024
+
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def minTaps(self, n, ranges):
+        """
+        :type n: int
+        :type ranges: List[int]
+        :rtype: int
+        """
+        def jump_game(A):
+            jump_count, reachable, curr_reachable = 0, 0, 0
+            for i, length in enumerate(A):
+                if i > reachable:
+                    return -1
+                if i > curr_reachable:
+                    curr_reachable = reachable
+                    jump_count += 1
+                reachable = max(reachable, i+length)
+            return jump_count
+    
+        max_range = [0]*(n+1)
+        for i, r in enumerate(ranges):
+            left, right = max(i-r, 0), min(i+r, n)
+            max_range[left] = max(max_range[left], right-left)
+        return jump_game(max_range)
+
