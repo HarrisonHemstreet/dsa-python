c4727dc2f3a33eb0928f55d08432d95f7407ab87
Harry <harrisonhemstreet@proton.me>
2024-01-11 07:44:01 -0700

    11 January 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -1179,3 +1179,43 @@ class Solution(object):
             max_range[left] = max(max_range[left], right-left)
         return jump_game(max_range)
 
+# 11 January 2024
+
+# Time:  O(nlogn) for total n addNums, O(logn) per addNum, O(1) per findMedian.
+# Space: O(n), total space
+
+from heapq import heappush, heappop
+
+class MedianFinder(object):
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.__max_heap = []
+        self.__min_heap = []
+
+    def addNum(self, num):
+        """
+        Adds a num into the data structure.
+        :type num: int
+        :rtype: void
+        """
+        # Balance smaller half and larger half.
+        if not self.__max_heap or num > -self.__max_heap[0]:
+            heappush(self.__min_heap, num)
+            if len(self.__min_heap) > len(self.__max_heap) + 1:
+                heappush(self.__max_heap, -heappop(self.__min_heap))
+        else:
+            heappush(self.__max_heap, -num)
+            if len(self.__max_heap) > len(self.__min_heap):
+                heappush(self.__min_heap, -heappop(self.__max_heap))
+
+    def findMedian(self):
+        """
+        Returns the median of current data stream
+        :rtype: float
+        """
+        return (-self.__max_heap[0] + self.__min_heap[0]) / 2.0 \
+               if len(self.__min_heap) == len(self.__max_heap) \
+               else self.__min_heap[0]
+
