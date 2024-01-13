e23082f324372e3a176aed4ae4acfe3e6323418f
Harry <harrisonhemstreet@proton.me>
2024-01-13 00:00:01 -0700

    13 January 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -1265,3 +1265,32 @@ class Solution_WA(object):
                     dp[j+1][0] = max(dp[j+1][0], dp[j+1][1]%2)
         return dp[-1][0] == 0
 
+# 13 January 2024
+
+# Time:  O(n)
+# Space: O(1)
+
+import itertools
+
+
+# greedy, kadane's algorithm
+class Solution(object):
+    def maximumCostSubstring(self, s, chars, vals):
+        """
+        :type s: str
+        :type chars: str
+        :type vals: List[int]
+        :rtype: int
+        """
+        def kadane(s):
+            result = curr = 0
+            for c in s:
+                curr = max(curr+(lookup[c] if c in lookup else ord(c)-ord('a')+1), 0)
+                result = max(result, curr)
+            return result
+
+        lookup = {}
+        for c, v in itertools.izip(chars, vals):
+            lookup[c] = v
+        return kadane(s)
+
