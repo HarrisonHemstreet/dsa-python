3f756d73241f169eaac5c04f4f56ce0d3eaeb1cd
Harry <harrisonhemstreet@proton.me>
2024-01-18 09:27:24 -0700

    18 January 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -1385,3 +1385,17 @@ class Solution(object):
             j = i
         return result if cnt and cnt%2 == 0 else 0
 
+# 18 January 2024
+
+# Time:  O(n)
+# Space: O(1)
+
+# string
+class Solution(object):
+    def isCircularSentence(self, sentence):
+        """
+        :type sentence: str
+        :rtype: bool
+        """
+        return sentence[0] == sentence[-1] and all(sentence[i-1] == sentence[i+1]for i in xrange(len(sentence)) if sentence[i] == ' ')
+
