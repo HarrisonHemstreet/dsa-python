de39aaaeb4bebf6658ef3b3057e6d21081043087
Harry <harrisonhemstreet@proton.me>
2024-01-19 07:11:14 -0700

    19 January 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -1399,3 +1399,38 @@ class Solution(object):
         """
         return sentence[0] == sentence[-1] and all(sentence[i-1] == sentence[i+1]for i in xrange(len(sentence)) if sentence[i] == ' ')
 
+# 19 January 2024
+
+# Time:  O(n)
+# Space: O(h)
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def minCameraCover(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        UNCOVERED, COVERED, CAMERA = range(3)
+        def dfs(root, result):
+            left = dfs(root.left, result) if root.left else COVERED
+            right = dfs(root.right, result) if root.right else COVERED
+            if left == UNCOVERED or right == UNCOVERED:
+                result[0] += 1
+                return CAMERA
+            if left == CAMERA or right == CAMERA:
+                return COVERED
+            return UNCOVERED
+        
+        result = [0]
+        if dfs(root, result) == UNCOVERED:
+            result[0] += 1
+        return result[0]
+
