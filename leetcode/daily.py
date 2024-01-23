056969238a3e590d86f349ac65bbbfb043462cd0
Harry <harrisonhemstreet@proton.me>
2024-01-23 09:11:56 -0700

    23 January 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -1487,3 +1487,38 @@ class Solution(object):
                 i += 1
         return True 
 
+# 23 January 2024
+
+# Time:  O(h)
+# Space: O(1)
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def insertIntoMaxTree(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return TreeNode(val)
+
+        if val > root.val:
+            node = TreeNode(val)
+            node.left = root
+            return node
+        
+        curr = root
+        while curr.right and curr.right.val > val:
+            curr = curr.right
+        node = TreeNode(val)
+        curr.right, node.left = node, curr.right
+        return root
+
