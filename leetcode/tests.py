1bb41c1ba55d4a30902468440f6eac7ba4f4301a
Harry <harrisonhemstreet@proton.me>
2024-01-23 19:31:58 -0700

    solve #643

--- leetcode/tests.py
+++ leetcode/tests.py
@@ -39,6 +39,7 @@ from move_zeroes import MoveZeroes
 from is_subsequence import IsSubsequence
 from max_water import MaxWater
 from max_ops import MaxOps
+from find_max_average import FindMaxAverage
 
 class TestSolution(unittest.TestCase):
     def setUp(self):
@@ -81,6 +82,7 @@ class TestSolution(unittest.TestCase):
         self.is_subsequence = IsSubsequence()
         self.max_water = MaxWater()
         self.max_ops = MaxOps()
+        self.find_max_average = FindMaxAverage()
 
     def test_two_sum(self):
         self.assertEqual(self.two_sum.run([2,7,11,15], 9), [0,1])
@@ -318,6 +320,16 @@ class TestSolution(unittest.TestCase):
     def test_max_ops(self):
         self.assertEqual(self.max_ops.run([1,2,3,4], 5), 2)
         self.assertEqual(self.max_ops.run([3,1,3,4,3], 6), 1)
+
+    def test_find_max_average(self):
+        self.assertEqual(self.find_max_average.run([1,12,-5,-6,50,3], 4), 12.75000)
+        self.assertEqual(self.find_max_average.run([5], 1), 5)
+        self.assertEqual(self.find_max_average.run([-1], 1), -1.0)
+        self.assertEqual(self.find_max_average.run(
+            [8860,-853,6534,4477,-4589,8646,-6155,-5577,-1656,-5779,-2619,-8604,-1358,-8009,4983,7063,3104,-1560,4080,2763,5616,-2375,2848,1394,-7173,-5225,-8244,-809,8025,-4072,-4391,-9579,1407,6700,2421,-6685,5481,-1732,-8892,-6645,3077,3287,-4149,8701,-4393,-9070,-1777,2237,-3253,-506,-4931,-7366,-8132,5406,-6300,-275,-1908,67,3569,1433,-7262,-437,8303,4498,-379,3054,-6285,4203,6908,4433,3077,2288,9733,-8067,3007,9725,9669,1362,-2561,-4225,5442,-9006,-429,160,-9234,-4444,3586,-5711,-9506,-79,-4418,-4348,-5891],
+            93
+        ), -594.5806451612904)
+        self.assertEqual(self.find_max_average.run([5], 1), 5)
 
 if __name__ == '__main__':
     unittest.main()
