129dbb3fcbb91a29afa87912dbca4b5db62b1518
Harry <harrisonhemstreet@proton.me>
2024-01-28 20:49:00 -0700

    28 January 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -1603,3 +1603,25 @@ class Solution(object):
         """
         return (arrivalTime + delayedTime)%24
 
+# 28 January 2024
+
+# Time:  O(n)
+# Space: O(1)
+
+import itertools
+
+
+class Solution(object):
+    def gridGame(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        result = float("inf")
+        left, right = 0, sum(grid[0])
+        for a, b in itertools.izip(grid[0], grid[1]):
+            right -= a
+            result = min(result, max(left, right))
+            left += b
+        return result
+
