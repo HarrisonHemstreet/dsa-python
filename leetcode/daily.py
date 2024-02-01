a75522af557a2efa22b9566e520cc499ec124579
Harry <harrisonhemstreet@proton.me>
2024-02-01 08:29:34 -0700

    01 February 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -1708,3 +1708,23 @@ class Solution(object):
                 return [mn_i, i+indexDifference]
         return [-1]*2
 
+# 01 February 2024
+
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    # @param ratings, a list of integer
+    # @return an integer
+    def candy(self, ratings):
+        candies = [1 for _ in xrange(len(ratings))]
+        for i in xrange(1, len(ratings)):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        for i in reversed(xrange(1, len(ratings))):
+            if ratings[i - 1] > ratings[i] and candies[i - 1] <= candies[i]:
+                candies[i - 1] = candies[i] + 1
+
+        return sum(candies)
+
