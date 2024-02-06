b03a6d86b1f4edc8cdaa89344661457ef6f5b5a1
Harry <harrisonhemstreet@proton.me>
2024-02-06 00:00:01 -0700

    06 February 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -1873,3 +1873,31 @@ class Solution(object):
                     rooms[I][J] = rooms[i][j] + 1
                     q.append((I, J))
 
+# 06 February 2024
+
+# Time:  O(n + q)
+# Space: O(n)
+
+class Solution(object):
+    def platesBetweenCandles(self, s, queries):
+        """
+        :type s: str
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        left, prefix = [0]*len(s), {}
+        curr, cnt = -1, 0
+        for i in xrange(len(s)):
+            if s[i] == '|':
+                curr = i
+                cnt += 1
+                prefix[i] = cnt
+            left[i] = curr
+        right = [0]*len(s)
+        curr = len(s)
+        for i in reversed(xrange(len(s))):
+            if s[i] == '|':
+                curr = i
+            right[i] = curr
+        return [max((left[r]-right[l]+1) - (prefix[left[r]]-prefix[right[l]]+1), 0) for l, r in queries]
+
