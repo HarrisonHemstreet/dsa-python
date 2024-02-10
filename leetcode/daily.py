868ba2321d23c6ae51f1e4ac97e6ac2cc0ca18b1
Harry <harrisonhemstreet@proton.me>
2024-02-10 00:00:01 -0700

    10 February 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -1979,3 +1979,28 @@ class Solution(object):
                 dec = True
         return not inc or not dec
 
+# 10 February 2024
+
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+
+
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        result = 0
+        accumulated_sum = 0
+        lookup = collections.defaultdict(int)
+        lookup[0] += 1
+        for num in nums:
+            accumulated_sum += num
+            result += lookup[accumulated_sum - k]
+            lookup[accumulated_sum] += 1
+        return result
+
