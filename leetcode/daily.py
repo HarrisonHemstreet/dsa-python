da8a2942b2fffb86c60b86856a9c7511a78f20df
Harry <harrisonhemstreet@proton.me>
2024-02-23 00:00:01 -0700

    23 February 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -2553,3 +2553,21 @@ class Solution(object):
             x, y = x1, y1
         return "".join(result)
 
+# 23 February 2024
+
+# Time:  O(n)
+# Space: O(n)
+
+# freq table
+class Solution(object):
+    def edgeScore(self, edges):
+        """
+        :type edges: List[int]
+        :rtype: int
+        """
+        score = [0]*len(edges)
+        for u, v in enumerate(edges):
+            score[v] += u
+        return max(xrange(len(edges)), key=lambda x:score[x])
+    
+
