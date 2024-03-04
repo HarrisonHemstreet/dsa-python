0a2628b60db8ef10d0c945fd22e9d1645d2d4fd7
Harry <harrisonhemstreet@proton.me>
2024-03-04 06:45:00 -0700

    04 March 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -2985,3 +2985,22 @@ class Solution(object):
 
         return backtracking(pieces, positions, 0, [[0]*8 for _ in range(8)])
 
+# 04 March 2024
+
+# Time:  O(n)
+# Space: O(1)
+
+# simulation
+class Solution(object):
+    def calculateTax(self, brackets, income):
+        """
+        :type brackets: List[List[int]]
+        :type income: int
+        :rtype: float
+        """
+        result = prev = 0
+        for u, p in brackets:
+            result += max((min(u, income)-prev)*p/100.0, 0.0)
+            prev = u
+        return result
+
