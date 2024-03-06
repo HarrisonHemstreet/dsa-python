f63c9a26710d5a12b5ee5aa47174261bc3873e5b
Harry <harrisonhemstreet@proton.me>
2024-03-06 08:21:53 -0700

    06 March 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -3030,3 +3030,18 @@ class Solution(object):
             dp = new_dp
         return dp[(1<<len(power))-1]
 
+# 06 March 2024
+
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def prefixesDivBy5(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[bool]
+        """
+        for i in xrange(1, len(A)):
+            A[i] += A[i-1] * 2 % 5
+        return [x % 5 == 0 for x in A]
+
