ea34c69da1a9f6e11c6737f0287f565e229d95e3
Harry <harrisonhemstreet@proton.me>
2024-03-11 00:00:01 -0600

    11 March 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -3355,3 +3355,26 @@ class Twitter(object):
         """
         self.__followings[followerId].discard(followeeId)
 
+# 11 March 2024
+
+# Time:  O(n)
+# Space: O(1)
+
+# greedy, math
+class Solution(object):
+    def minimumReplacement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def ceil_divide(a, b):
+            return (a+b-1)//b
+
+        result = 0
+        curr = nums[-1]
+        for x in reversed(nums):
+            cnt = ceil_divide(x, curr)
+            result += cnt-1
+            curr = x//cnt
+        return result
+
