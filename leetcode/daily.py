bc3f8d290a29dfa3762175fe1159dd7220f500c2
Harry <harrisonhemstreet@proton.me>
2024-03-12 00:00:01 -0600

    12 March 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -3378,3 +3378,23 @@ class Solution(object):
             curr = x//cnt
         return result
 
+# 12 March 2024
+
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def makeGood(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stk = []
+        for ch in s:
+            counter_ch = ch.upper() if ch.islower() else ch.lower()
+            if stk and stk[-1] == counter_ch:
+                stk.pop()
+            else:
+                stk.append(ch)
+        return "".join(stk)
+
