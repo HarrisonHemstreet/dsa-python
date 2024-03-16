3381be9e876d8a8df843db4aac78ea6aee291ad4
Harry <harrisonhemstreet@proton.me>
2024-03-16 00:00:02 -0600

    16 March 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -3629,3 +3629,23 @@ class Solution2(object):
         #             q2.append((ni, nj, nt))
         return color[MOUSE_START, CAT_START, MOUSE] == MOUSE
 
+# 16 March 2024
+
+# Time:  O(n + m)
+# Space: O(1)
+
+import collections
+
+
+# freq table
+class Solution(object):
+    def rearrangeCharacters(self, s, target):
+        """
+        :type s: str
+        :type target: str
+        :rtype: int
+        """
+        cnt1 = collections.Counter(s)
+        cnt2 = collections.Counter(target)
+        return min(cnt1[k]//v for k, v in cnt2.iteritems())
+
