78674ea61bcc80a110f1f44985763fb59850cdd4
Harry <harrisonhemstreet@proton.me>
2024-03-29 00:00:01 -0600

    29 March 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -4098,3 +4098,23 @@ class Solution2(object):
         y = min(abs(y1), abs(y2)) if y1*y2 > 0 else 0
         return x**2 + y**2 <= radius**2
 
+# 29 March 2024
+
+# Time:  O(1)
+# Space: O(1)
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        pass
+
+
+# tree
+class Solution(object):
+    def checkTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        return root.val == root.left.val+root.right.val
+
