8efd146a6d80f3b574d5caf8e70b4c3dc6fe3d46
Harry <harrisonhemstreet@proton.me>
2024-03-30 00:00:01 -0600

    30 March 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -4118,3 +4118,23 @@ class Solution(object):
         """
         return root.val == root.left.val+root.right.val
 
+# 30 March 2024
+
+# Time:  O(n)
+# Space: O(1)
+
+# math
+class Solution(object):
+    def averageValue(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total = cnt = 0
+        for x in nums:
+            if x%6:
+                continue
+            total += x
+            cnt += 1
+        return total//cnt if cnt else 0
+
