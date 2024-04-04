ee5d439b1e2769f0fc555c8d73fad8661d9dc18a
Harry <harrisonhemstreet@proton.me>
2024-04-04 00:00:01 -0600

    04 April 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -4305,3 +4305,21 @@ class Solution(object):
         nth_element(vals, k-1, compare=lambda a, b: a > b)
         return vals[k-1]
 
+# 04 April 2024
+
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def movesToMakeZigzag(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = [0, 0]
+        for i in xrange(len(nums)):
+            left = nums[i-1] if i-1 >= 0 else float("inf")
+            right = nums[i+1] if i+1 < len(nums) else float("inf")
+            result[i%2] += max(nums[i] - min(left, right) + 1, 0)
+        return min(result)
+
