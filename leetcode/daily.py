de165d023ad83a69db27018a88addb706280b19b
Harry <harrisonhemstreet@proton.me>
2024-04-08 00:00:01 -0600

    08 April 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -4443,3 +4443,23 @@ class Solution(object):
                 result = min(result, n)
         return result if result < float("inf") else 0
 
+# 08 April 2024
+
+# Time:  O(logn)
+# Space: O(1)
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1, n
+        while left <= right:
+            mid = left + (right - left) / 2
+            if isBadVersion(mid): # noqa
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
+
