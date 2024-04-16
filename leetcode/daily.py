3de9778aa44ca0e64e2dd4a76265ec60067a139f
Harry <harrisonhemstreet@proton.me>
2024-04-16 00:00:02 -0600

    16 April 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -4670,3 +4670,37 @@ class Codec(object):
             i += 8+l
         return strs
 
+# 16 April 2024
+
+# Time:  O(n + m), m is the max number of nums
+# Space: O(m)
+
+import collections
+
+
+class Solution(object):
+    def smallerNumbersThanCurrent(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        count = collections.Counter(nums)
+        for i in xrange(max(nums)+1):
+            count[i] += count[i-1]
+        return [count[i-1] for i in nums]
+
+
+# Time:  O(nlogn)
+# Space: O(n)
+import bisect
+
+
+class Solution2(object):
+    def smallerNumbersThanCurrent(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        sorted_nums = sorted(nums)
+        return [bisect.bisect_left(sorted_nums, i) for i in nums]
+
