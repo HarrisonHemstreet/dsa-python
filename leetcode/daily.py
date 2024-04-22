b7e745e3584f5208690801fd2c0b15da87f973da
Harry <harrisonhemstreet@proton.me>
2024-04-22 09:01:02 -0600

    22 April 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -4851,3 +4851,21 @@ class Solution(object):
         return result
             
 
+# 22 April 2024
+
+# Time:  O(n)
+# Space: O(1)
+
+from operator import xor
+from functools import reduce
+
+
+class Solution(object):
+    def xorGame(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return reduce(xor, nums) == 0 or \
+            len(nums) % 2 == 0
+
