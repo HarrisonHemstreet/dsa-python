6ed922a70593eda2caee2f46e11a1e6c950ec101
Harry <harrisonhemstreet@proton.me>
2024-04-26 09:00:50 -0600

    26 April 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -5130,3 +5130,20 @@ class Solution(object):
 
         return i
 
+# 26 April 2024
+
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def findPermutation(self, s):
+        """
+        :type s: str
+        :rtype: List[int]
+        """
+        result = []
+        for i in xrange(len(s)+1):
+            if i == len(s) or s[i] == 'I':
+                result += range(i+1, len(result), -1)
+        return result
+
