536cc0851d5a4c9d5d9f6ca58f3a4cff870496c3
Harry <harrisonhemstreet@proton.me>
2024-04-30 00:00:01 -0600

    30 April 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -5264,3 +5264,23 @@ class Solution(object):
             dp[i%W] = min(dp[j%W] for j in xrange(i-W, i))+max(k-x, 0)
         return min(dp)
 
+# 30 April 2024
+
+# Time:  O(q * n)
+# Space: O(1)
+
+class Solution(object):
+    def countPoints(self, points, queries):
+        """
+        :type points: List[List[int]]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        result = []
+        for i, j, r in queries:
+            result.append(0)
+            for x, y in points:
+                if (x-i)**2+(y-j)**2 <= r**2:
+                    result[-1] += 1
+        return result
+
