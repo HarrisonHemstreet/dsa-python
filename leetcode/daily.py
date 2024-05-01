ab1403fec77d18b8cbe4229a45a9afc36e4906db
Harry <harrisonhemstreet@proton.me>
2024-05-01 09:01:54 -0600

    01 May 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -5284,3 +5284,22 @@ class Solution(object):
                     result[-1] += 1
         return result
 
+# 01 May 2024
+
+# Time:  O(n)
+# Space: O(1)
+
+# two pointers
+class Solution(object):
+    def longestSemiRepetitiveSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = left = prev = 0
+        for right in xrange(len(s)):
+            if right-1 >= 0 and s[right-1] == s[right]:
+                left, prev = prev, right
+            result = max(result, right-left+1)
+        return result
+
