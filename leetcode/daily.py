7f4982ba12d1e73c5ef6076e3e1835617c95c6c8
Harry <harrisonhemstreet@proton.me>
2024-05-03 00:00:01 -0600

    03 May 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -5323,3 +5323,22 @@ class Solution(object):
                 result.append(c)
         return "".join(result)
 
+# 03 May 2024
+
+# Time:  O(n)
+# Space: O(1)
+
+import operator
+
+
+# bit manipulation
+class Solution(object):
+    def xorAllNums(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        return (reduce(operator.xor, nums1) if len(nums2)%2 else 0) ^ \
+               (reduce(operator.xor, nums2) if len(nums1)%2 else 0)
+
