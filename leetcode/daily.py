8f24a33d6d22020f586599bb4c15dcb04a50e76d
Harry <harrisonhemstreet@proton.me>
2024-05-06 00:00:01 -0600

    06 May 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -5362,3 +5362,17 @@ class Solution(object):
                 return False
         return True
 
+# 06 May 2024
+
+# Time:  O(1)
+# Space: O(1)
+
+# math
+class Solution(object):
+    def convertTemperature(self, celsius):
+        """
+        :type celsius: float
+        :rtype: List[float]
+        """
+        return [celsius+273.15, celsius*1.80+32.00]
+
