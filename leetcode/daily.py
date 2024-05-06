01b35b2810bb6744ea4791352c6f936bb77ccdc2
Harry <harrisonhemstreet@proton.me>
2024-05-05 18:53:29 -0600

    05 May 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -5342,3 +5342,23 @@ class Solution(object):
         return (reduce(operator.xor, nums1) if len(nums2)%2 else 0) ^ \
                (reduce(operator.xor, nums2) if len(nums1)%2 else 0)
 
+# 05 May 2024
+
+# Time:  O(nlogn)
+# Space: O(n)
+
+class Solution(object):
+    def carPooling(self, trips, capacity):
+        """
+        :type trips: List[List[int]]
+        :type capacity: int
+        :rtype: bool
+        """
+        line = [x for num, start, end in trips for x in [[start, num], [end, -num]]]
+        line.sort()
+        for _, num in line:
+            capacity -= num
+            if capacity < 0:
+                return False
+        return True
+
