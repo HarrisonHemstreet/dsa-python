9d3827bf3225eab14073197cd56d15da81cef518
Harry <harrisonhemstreet@proton.me>
2024-05-11 00:00:01 -0600

    11 May 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -5526,3 +5526,22 @@ class Solution(object):
                     result = min(result, abs(A) * abs(P-Q))
         return result if result < float("inf") else 0.0
 
+# 11 May 2024
+
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def maxAscendingSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = curr = 0
+        for i in xrange(len(nums)): 
+            if not (i and nums[i-1] < nums[i]):
+                curr = 0
+            curr += nums[i]
+            result = max(result, curr)
+        return result
+
