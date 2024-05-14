581270409e093208c0035dbb5da6540a49616b7e
Harry <harrisonhemstreet@proton.me>
2024-05-14 00:00:01 -0600

    14 May 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -5595,3 +5595,24 @@ class Solution(object):
                     return True
         return False
 
+# 14 May 2024
+
+# Time:  O(n)
+# Space: O(1)
+
+# sliding window, two pointers
+class Solution(object):
+    def longestNiceSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = left = curr = 0
+        for right in xrange(len(nums)):
+            while curr&nums[right]:
+                curr ^= nums[left]
+                left += 1
+            curr |= nums[right]
+            result = max(result, right-left+1)
+        return result
+
