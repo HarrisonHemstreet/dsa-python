1c64b1383656505d3491d4f0ddf018f95796398b
Harry <harrisonhemstreet@proton.me>
2024-05-17 00:00:01 -0600

    17 May 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -5670,3 +5670,21 @@ class Solution(object):
 
         return True
 
+# 17 May 2024
+
+# Time:  O(m + n)
+# Space: O(1)
+
+class Solution(object):
+    def countNegatives(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        result, c = 0, len(grid[0])-1
+        for row in grid:
+            while c >= 0 and row[c] < 0:
+                c -= 1
+            result += len(grid[0])-1-c
+        return result
+
