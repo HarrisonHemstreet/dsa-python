00d2cb4ab39bb6bf5f5f38b3b2eea2d1f0031441
Harry <harrisonhemstreet@proton.me>
2024-06-04 00:00:01 -0600

    04 June 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -5949,3 +5949,38 @@ class Solution2(object):
                 rmq[j][i] = max(rmq[j][i+1], (prefix[j+1]-prefix[i]) + max_score)
         return dp[0][n-1]
 
+# 04 June 2024
+
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def removeZeroSumSublists(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        curr = dummy = ListNode(0)
+        dummy.next = head
+        prefix = 0
+        lookup = collections.OrderedDict()
+        while curr:
+            prefix += curr.val
+            node = lookup.get(prefix, curr)
+            while prefix in lookup:
+                lookup.popitem()
+            lookup[prefix] = node
+            node.next = curr.next
+            curr = curr.next
+        return dummy.next
+
