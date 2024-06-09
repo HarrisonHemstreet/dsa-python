faca11ba497232827a26c921a316d17eb991ff2b
Harry <harrisonhemstreet@proton.me>
2024-06-09 00:00:01 -0600

    09 June 2024

--- leetcode/daily.py
+++ leetcode/daily.py
@@ -6120,3 +6120,28 @@ class Solution(object):
 
         return backtracking(pieces, positions, 0, [[0]*8 for _ in range(8)])
 
+# 09 June 2024
+
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+
+
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        result = 0
+        accumulated_sum = 0
+        lookup = collections.defaultdict(int)
+        lookup[0] += 1
+        for num in nums:
+            accumulated_sum += num
+            result += lookup[accumulated_sum - k]
+            lookup[accumulated_sum] += 1
+        return result
+
